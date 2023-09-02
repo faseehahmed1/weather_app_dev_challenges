b5c5c8fb8866342882400daf30390e66f317039e
faseehahmed1 <faseeh_455@hotmail.com>
2023-09-02 09:59:25 +0100

    base repo added to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/weather_app_dev_challenges",
   plugins: [react()],
   server: {
     port: 8000,
